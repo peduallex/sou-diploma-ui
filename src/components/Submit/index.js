b13cc5f236b35dc741195a87d31ebe7d6777a853
rubenssamara <rubens.samara@connectcom.univesp.br>
2019-01-07 11:59:12 -0200

    Correção de sintaxe do evento onClick no component Sumbimit.js

--- src/components/Submit/index.js
+++ src/components/Submit/index.js
@@ -17,7 +17,7 @@ class Submit extends Component {
               <li className="li">
                 <a
                   className="a"
-                  onClick={this.handleClick(1, 'ANDREA GONÇALVES')}
+                  onClick={() => this.handleClick(1, 'ANDREA GONÇALVES')}
                 >
                   ANDREA GONÇALVES MARIANO SOUZA (2 processos)
                 </a>
@@ -25,7 +25,7 @@ class Submit extends Component {
               <li className="li">
                 <a
                   className="a"
-                  onClick={this.handleClick(2, 'CAIO GUILHERME')}
+                  onClick={() => this.handleClick(2, 'CAIO GUILHERME')}
                 >
                   CAIO GUILHERME SOARES FERNANDES (1 processos)
                 </a>
@@ -33,7 +33,7 @@ class Submit extends Component {
               <li className="li">
                 <a
                   className="a"
-                  onClick={this.handleClick(3, 'GENIVALDO LINHARES')}
+                  onClick={() => this.handleClick(3, 'GENIVALDO LINHARES')}
                 >
                   GENIVALDO LINHARES BRANDÃO (4 processos)
                 </a>
@@ -41,7 +41,7 @@ class Submit extends Component {
               <li className="li">
                 <a
                   className="a"
-                  onClick={this.handleClick(4, 'HAROLDO FELIPE')}
+                  onClick={() => this.handleClick(4, 'HAROLDO FELIPE')}
                 >
                   HAROLDO FELIPPE AVELLAR (2 processos)
                 </a>
@@ -49,13 +49,13 @@ class Submit extends Component {
               <li className="li">
                 <a
                   className="a"
-                  onClick={this.handleClick(5, 'LEILA MIGUELINA')}
+                  onClick={() => this.handleClick(5, 'LEILA MIGUELINA')}
                 >
                   LEILA MIGUELINA APARECIDA COSTA SOMENK (2 processos)
                 </a>
               </li>
               <li className="li">
-                <a className="a" onClick={this.handleClick(6, 'MARCIO ROCHA')}>
+                <a className="a" onClick={() => this.handleClick(6, 'MARCIO ROCHA')}>
                   MARCIO ROCHA DE PINHO (2 processos)
                 </a>
               </li>
